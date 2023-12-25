bb0691b86405216a0c0ac7e9245a37bee378a7a4
Imo Madjid <mhmmadjid@gmail.com>
2023-12-25 20:17:31 +0700

    feat: initial commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    "prettier-plugin-organize-attributes",
+    "prettier-plugin-organize-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  tailwindFunctions: ["clsx", "cn", "cva"],
+};
